4d669fbf1a3dd84c12f88c5f10f0f39b23400141
Jenish <hello@jenish.net>
2024-01-03 17:31:30 +0530

    Promperties updated with JWT Key[]

--- src/main/java/com/book/security/service/JwtService.java
+++ src/main/java/com/book/security/service/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
+    @Value("${jwt.key}")
     private final static String SECRET_Key="655368566D597133743677397A24432646294A404E635266546A576E5A723475";
 
 
